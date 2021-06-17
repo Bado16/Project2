24f25a4249a2f6bbe7e8038c5c76fbfd940f9034
Bahadr öztürk <bahadrozturk88@gmail.com>
2021-06-17 17:54:13 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Cats = new MyDictionary<int, string>();
+            Cats.Add(3, "Persian");
+            Cats.Add(5, "British Short Hair");
+            Cats.Add(1, "Scottish Fold");
+            Cats.Add(2, "Lion");
+            Cats.Add(4, "Tiger");
+
+           
+        }
+    }
+}
